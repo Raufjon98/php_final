48951b5ab3ec67dae7ba9d9858efa1d08a885a2e
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2023-08-03 20:48:59 +0900

    Its not Directory

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?
 function getConnection() 
 {
-    $var = 'working Directory';
+    $var = 'It`s not Directory';
     try {
         $connection = new PDO('mysql:host=localhost; dbname=cloud_storage', 'root', '6260834700109');
         if($connection){
