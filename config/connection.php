01c7514d5ccb67e65f287b98f1eb61e06e556daf
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2023-08-03 20:35:30 +0900

    Test github changes

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,7 @@
 <?
 function getConnection() 
 {
+    $var = 'Test github changes';
     try {
         $connection = new PDO('mysql:host=localhost; dbname=cloud_storage', 'root', '6260834700109');
         if($connection){
